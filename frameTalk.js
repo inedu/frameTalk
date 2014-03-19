ad410e40b7c2c059b20ed620d97e01facb0f278d
johnpan <johnnakis@gmail.com>
2014-03-19 13:17:36 +0200

    handshake muted

--- frameTalk.js
+++ frameTalk.js
@@ -1,10 +1,11 @@
 // 1. put the code to both DOMs (window + iFrame)
-// 2. run both frameTalk.initListener()
+// 2. run both frameTalk.init()
+// sendMessage first param must be a window object. Try .contentWindow for iFrames
 // **** send example: frameTalk.sendMessage( window.top, { "theFunction" : "doRun", "theParams" : [1,2,3,'four'] });
 
 (function (window) {
     "use strict";
-    var frameTalk, handShakeReplyTo;    
+    var frameTalk;    
 	
 	frameTalk = {
         init : function() {
@@ -19,12 +20,16 @@
                 say("JSON missing");
             }
         },
-    
-		handshake : function (from, to) {
-			handShakeReplyTo = from;
-			frameTalk.sendMessage(to, { "theFunction": "handshake", "theParams": [0] });
-		},
-	
+		 
+		/*handshake : function (fromWindow, toWindow) {
+			if (typeof fromWindow != "object" || !fromWindow.postMessage || 
+				typeof toWindow != "object" || !toWindow.postMessage ) {
+					say('handshake needs to window objects with postMessage defined');
+					return; 
+			}
+			frameTalk.sendMessage(to, { "theFunction": "handshake", "theParams": from });
+		},*/
+		 
         sendMessage : function (where, theMessage) {
             try {
 				if (!theMessage.theFunction || typeof theMessage.theFunction != "string" ||
@@ -32,6 +37,10 @@
 						say("theMessage.theFunction must be a function's name (string), and theMessage.theParams must be an array");
 						return;
 				}
+				if (typeof where != "object" || !where.postMessage ) {
+					say("sendMessage first param must be a window object with postMessage defined. Try .contentWindow for iFrames");
+					return;
+				} 
                 // some browsers do not support json via postMessage, so stringify                                   
                 var myMsg = window.JSON.stringify(theMessage);
                 where.postMessage(myMsg, '*');
@@ -51,10 +60,7 @@
                     frameTalk.sendMessage(handShakeReplyTo, { "theFunction": "replyHandshake", "theParams": [1] });
                 }  
                 if (theFunction == "replyHandshake") {           
-                    if (theParams[0] == 1) { 
-						handShakeReplyTo = null;
-						say("HandShake completed." ); 
-					}
+                    if (theParams[0] == 1) { say("HandShake completed." ); }
                 }                  
                 else {
 					// call the function that other iFrame asked to
