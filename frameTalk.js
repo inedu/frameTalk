373bd2d7d0e0a9176c65037578fbecf2b2d77b94
johnpan <johnnakis@gmail.com>
2014-03-20 15:07:18 +0200

    init, handshake try1
    init avoids double listen, sendMessage accepts single var param

--- frameTalk.js
+++ frameTalk.js
@@ -5,33 +5,39 @@
 
 (function (window) {
     "use strict";
-    var frameTalk;    
+    var frameTalk, hasBeenInit = false;    
 	
 	frameTalk = {
         init : function() {
-            if (window.addEventListener) {
-                window.addEventListener("message", receiveMessage, false);       
-            } else if (window.attachEvent) {
-                window.attachEvent("onmessage", receiveMessage);        
-            } else {
-                say("could not attach event listener");
-            }
-            if (! (window.JSON && window.JSON.parse && window.JSON.stringify)) {
-                say("JSON missing, please load JSON2");
-            }
+			if (! (window.JSON && window.JSON.parse && window.JSON.stringify)) {
+                say("No init, JSON missing, please load JSON2");
+				return;
+            }		
+			if (!hasBeenInit) {
+				if (window.addEventListener) {
+					window.addEventListener("message", receiveMessage, false); 
+					hasBeenInit = true;
+				} else if (window.attachEvent) {
+					window.attachEvent("onmessage", receiveMessage);   
+					hasBeenInit = true;					
+				} else { say("could not attach event listener"); }
+			} else { say("already init"); }
         },		 
 		
 		sendMessage : function (where, theFunction, theParams) {
             try {
-				if (typeof theFunction != "string" ||
-					typeof theParams != "object" ) {
-						say("theFunction must be a function's name (string), and theParams must be an array");
-						return;
+				if (typeof theFunction != "string" ) {
+					say("sendMessage second param must be a function's name (string)");
+					return;
 				}
 				if (typeof where != "object" || !where.postMessage ) {
 					say("sendMessage first param must be a window object with postMessage defined. Try .contentWindow for iFrames");
 					return;
 				} 
+				if (typeof theParams != "object" ) {
+					// turn theParams into single record array
+					theParams = [theParams];
+				}
                 // some browsers do not support json via postMessage, so stringify                                   
                 var myMsgObj = {"theFunction":theFunction, "theParams":theParams};
 				var myMsg = window.JSON.stringify(myMsgObj);
@@ -39,30 +45,59 @@
             } catch (err) {
                 say("sendMessage Error - description: " + err.message);        
             }
-        }
+        },
 		 
-        /*handshake : function (fromWindow, toWindow) {
-			if (typeof fromWindow != "object" || !fromWindow.postMessage || 
-				typeof toWindow != "object" || !toWindow.postMessage ) {
-					say('handshake needs to window objects with postMessage defined');
-					return; 
+        handshake : function (toWindow) {
+			var windowFromName;
+			if ( typeof toWindow != "object" || !toWindow.postMessage ) {
+				say('handshake needs a window object with postMessage defined');
+				return; 
 			}
-			frameTalk.sendMessage(to, { "theFunction": "handshake", "theParams": from });
-		};*/
+			if (window.top === window) {
+				// handshake starts from top window
+				windowFromName = "@@top@@"; 
+			} else {
+				windowFromName = window.name;
+			}
+			frameTalk.sendMessage(toWindow, "handshake", [windowFromName]);
+		}
     };    
 
 	function receiveMessage (event) {
 		try {
 			// sendMessage always sends a string, so, turn it into json
-			var eventObjData = window.JSON.parse(event.data);
-			var theFunction = eventObjData.theFunction;
-			var theParams = eventObjData.theParams;
+			var eventObjData = window.JSON.parse(event.data),
+				theFunction = eventObjData.theFunction,
+				theParams = eventObjData.theParams,
+				wObj;
 			//
 			if (theFunction == "handshake") { 
-				frameTalk.sendMessage(handShakeReplyTo, { "theFunction": "replyHandshake", "theParams": [1] });
+				windowNameToReply = theParams[0]; 
+				if (windowNameToReply === "@@top@@") {
+					wObj = window.top;
+					frameTalk.sendMessage(wObj, { "theFunction": "replyHandshake", "theParams": [0] });
+				} else {
+					wObj = window.document.getElementById(windowNameToReply);					
+					if ( wObj && wObj.contentWindow ) {
+						// it's an iFrame. Put [1] in params
+						frameTalk.sendMessage(wObj.contentWindow, { "theFunction": "replyHandshake", "theParams": [1] });
+					} else {
+						// it's not the top window nor an iFrame in this window. Look for iFrames in parent (nested case)
+						wObj = window.parent.document.getElementById(windowNameToReply);
+						if ( wObj && wObj.contentWindow ) {
+							frameTalk.sendMessage(wObj.contentWindow, { "theFunction": "replyHandshake", "theParams": [1] });
+						} else {
+							say("could not find handshake receiver");
+						}
+					}
+				}				 
 			}  
 			if (theFunction == "replyHandshake") {           
-				if (theParams[0] == 1) { say("HandShake completed." ); }
+				if (theParams[0] === 0) { 
+					say("HandShake with top window completed." ); 
+				} else {
+					say("HandShake completed." ); 
+				}
 			}                  
 			else {
 				// call the function that other iFrame asked to
